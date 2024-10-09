76db8a0084ae4796d4943b77eadcbc7fec352862
David Gallmeier <dg@xounges.net>
2024-10-09 00:15:45 -0400

    add cloud-init

--- tf/.terraform.lock.hcl
+++ tf/.terraform.lock.hcl
@@ -21,3 +21,58 @@ provider "registry.terraform.io/dmacvicar/libvirt" {
     "zh:fb089d5da49e6024b128c3bfbe5d47f150fd288ee2689cad0f14a6ae1a173f9d",
   ]
 }
+
+provider "registry.terraform.io/hashicorp/local" {
+  version = "2.5.2"
+  hashes = [
+    "h1:JlMZD6nYqJ8sSrFfEAH0Vk/SL8WLZRmFaMUF9PJK5wM=",
+    "zh:136299545178ce281c56f36965bf91c35407c11897f7082b3b983d86cb79b511",
+    "zh:3b4486858aa9cb8163378722b642c57c529b6c64bfbfc9461d940a84cd66ebea",
+    "zh:4855ee628ead847741aa4f4fc9bed50cfdbf197f2912775dd9fe7bc43fa077c0",
+    "zh:4b8cd2583d1edcac4011caafe8afb7a95e8110a607a1d5fb87d921178074a69b",
+    "zh:52084ddaff8c8cd3f9e7bcb7ce4dc1eab00602912c96da43c29b4762dc376038",
+    "zh:71562d330d3f92d79b2952ffdda0dad167e952e46200c767dd30c6af8d7c0ed3",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:805f81ade06ff68fa8b908d31892eaed5c180ae031c77ad35f82cb7a74b97cf4",
+    "zh:8b6b3ebeaaa8e38dd04e56996abe80db9be6f4c1df75ac3cccc77642899bd464",
+    "zh:ad07750576b99248037b897de71113cc19b1a8d0bc235eb99173cc83d0de3b1b",
+    "zh:b9f1c3bfadb74068f5c205292badb0661e17ac05eb23bfe8bd809691e4583d0e",
+    "zh:cc4cbcd67414fefb111c1bf7ab0bc4beb8c0b553d01719ad17de9a047adff4d1",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/template" {
+  version = "2.2.0"
+  hashes = [
+    "h1:94qn780bi1qjrbC3uQtjJh3Wkfwd5+tTtJHOb7KTg9w=",
+    "zh:01702196f0a0492ec07917db7aaa595843d8f171dc195f4c988d2ffca2a06386",
+    "zh:09aae3da826ba3d7df69efeb25d146a1de0d03e951d35019a0f80e4f58c89b53",
+    "zh:09ba83c0625b6fe0a954da6fbd0c355ac0b7f07f86c91a2a97849140fea49603",
+    "zh:0e3a6c8e16f17f19010accd0844187d524580d9fdb0731f675ffcf4afba03d16",
+    "zh:45f2c594b6f2f34ea663704cc72048b212fe7d16fb4cfd959365fa997228a776",
+    "zh:77ea3e5a0446784d77114b5e851c970a3dde1e08fa6de38210b8385d7605d451",
+    "zh:8a154388f3708e3df5a69122a23bdfaf760a523788a5081976b3d5616f7d30ae",
+    "zh:992843002f2db5a11e626b3fc23dc0c87ad3729b3b3cff08e32ffb3df97edbde",
+    "zh:ad906f4cebd3ec5e43d5cd6dc8f4c5c9cc3b33d2243c89c5fc18f97f7277b51d",
+    "zh:c979425ddb256511137ecd093e23283234da0154b7fa8b21c2687182d9aea8b2",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/time" {
+  version = "0.12.1"
+  hashes = [
+    "h1:6BhxSYBJdBBKyuqatOGkuPKVenfx6UmLdiI13Pb3his=",
+    "zh:090023137df8effe8804e81c65f636dadf8f9d35b79c3afff282d39367ba44b2",
+    "zh:26f1e458358ba55f6558613f1427dcfa6ae2be5119b722d0b3adb27cd001efea",
+    "zh:272ccc73a03384b72b964918c7afeb22c2e6be22460d92b150aaf28f29a7d511",
+    "zh:438b8c74f5ed62fe921bd1078abe628a6675e44912933100ea4fa26863e340e9",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:85c8bd8eefc4afc33445de2ee7fbf33a7807bc34eb3734b8eefa4e98e4cddf38",
+    "zh:98bbe309c9ff5b2352de6a047e0ec6c7e3764b4ed3dfd370839c4be2fbfff869",
+    "zh:9c7bf8c56da1b124e0e2f3210a1915e778bab2be924481af684695b52672891e",
+    "zh:d2200f7f6ab8ecb8373cda796b864ad4867f5c255cff9d3b032f666e4c78f625",
+    "zh:d8c7926feaddfdc08d5ebb41b03445166df8c125417b28d64712dccd9feef136",
+    "zh:e2412a192fc340c61b373d6c20c9d805d7d3dee6c720c34db23c2a8ff0abd71b",
+    "zh:e6ac6bba391afe728a099df344dbd6481425b06d61697522017b8f7a59957d44",
+  ]
+}
--- tf/libvirt.tf
+++ tf/libvirt.tf
@@ -6,25 +6,6 @@ resource "libvirt_pool" "default" {
   # permissions issues ref: https://github.com/dmacvicar/terraform-provider-libvirt/issues/658#issuecomment-569695035
 }
 
-resource "libvirt_volume" "debian-bookworm-qcow2" {
-  name   = "debian-bookworm.qcow2"
-  source = "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-nocloud-amd64.qcow2"
-  format = "qcow2"
-  pool   = libvirt_pool.default.name # List storage pools using virsh pool-list
-}
-#
-resource "libvirt_volume" "debian-bookworm-qcow2-server" {
-  name   = "debian-bookworm-server.qcow2"
-  base_volume_id = libvirt_volume.debian-bookworm-qcow2.id
-  base_volume_pool = libvirt_volume.debian-bookworm-qcow2.pool
-}
-
-resource "libvirt_volume" "debian-bookworm-qcow2-agent0" {
-  name   = "debian-bookworm-agent0.qcow2"
-  base_volume_id = libvirt_volume.debian-bookworm-qcow2.id
-  base_volume_pool = libvirt_volume.debian-bookworm-qcow2.pool
-}
-
 resource "libvirt_network" "kube_network" {
   name   = "k8snet"
   mode   = "nat"
@@ -40,53 +21,61 @@ resource "libvirt_network" "kube_network" {
   autostart = true
 }
 
-# resource "libvirt_domain" "k8s-server" {
-#   name   = "k8s-server"
-#   memory = "2048"
-#   vcpu   = 2
-#   network_interface {
-#     network_name = libvirt_network.kube_network.name # List networks with virsh net-list
-#   }
-#   disk {
-#     volume_id = libvirt_volume.debian-bookworm-qcow2.id
-#   }
-#   console {
-#     type        = "pty"
-#     target_type = "serial"
-#     target_port = "0"
-#   }
-#   graphics {
-#     type        = "spice"
-#     listen_type = "address"
-#     autoport    = true
-#   }
-# }
+resource "libvirt_volume" "debian-bookworm-qcow2" {
+  name   = "debian-bookworm.qcow2"
+  source = "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2"
+  format = "qcow2"
+  pool   = libvirt_pool.default.name # List storage pools using virsh pool-list
+}
 #
-# resource "libvirt_domain" "k8s-agent0" {
-#   name   = "k8s-agent0"
-#   memory = "2048"
-#   vcpu   = 2
-#   network_interface {
-#     network_name = libvirt_network.kube_network.name # List networks with virsh net-list
-#   }
-#   disk {
-#     volume_id = libvirt_volume.debian-bookworm-qcow2-agent.id
-#   }
-#   console {
-#     type        = "pty"
-#     target_type = "serial"
-#     target_port = "0"
-#   }
-#   graphics {
-#     type        = "spice"
-#     listen_type = "address"
-#     autoport    = true
-#   }
-# }
+resource "libvirt_volume" "debian-bookworm-qcow2-server" {
+  name             = "debian-bookworm-server.qcow2"
+  base_volume_id   = libvirt_volume.debian-bookworm-qcow2.id
+  base_volume_pool = libvirt_volume.debian-bookworm-qcow2.pool
+}
 
-# output "ips" {
-#   value = {
-#     server = libvirt_domain.k8s-server.network_interface.0.addresses.0
-#     agent0 = libvirt_domain.k8s-agent0.network_interface.0.addresses.0
-#   }
-# }
+resource "libvirt_volume" "debian-bookworm-qcow2-agent0" {
+  name             = "debian-bookworm-agent0.qcow2"
+  base_volume_id   = libvirt_volume.debian-bookworm-qcow2.id
+  base_volume_pool = libvirt_volume.debian-bookworm-qcow2.pool
+}
+
+data "template_file" "user_data" {
+  template = file("${path.module}/cloud_init.cfg")
+}
+
+resource "libvirt_cloudinit_disk" "commoninit" {
+  name      = "commoninit.iso"
+  pool      = libvirt_pool.default.name
+  user_data = data.template_file.user_data.rendered
+}
+
+resource "libvirt_domain" "k8s-server" {
+  name   = "k8s-server"
+  memory = "2048"
+  vcpu   = 2
+  network_interface {
+    network_name = libvirt_network.kube_network.name # List networks with virsh net-list
+  }
+  disk {
+    volume_id = libvirt_volume.debian-bookworm-qcow2-server.id
+  }
+  cloudinit = libvirt_cloudinit_disk.commoninit.id
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+}
+
+output "ips" {
+  value = {
+    #server = libvirt_domain.k8s-server.network_interface.0.addresses.0
+    # agent0 = libvirt_domain.k8s-agent0.network_interface.0.addresses.0
+  }
+}
