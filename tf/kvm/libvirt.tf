3e8db97c9dd9807adca1f3a54cb36fd6d3d91497
David Gallmeier <dg@xounges.net>
2024-10-10 13:41:32 -0400

    refactor to module

--- tf/kvm/libvirt.tf
+++ tf/kvm/libvirt.tf
@@ -27,15 +27,9 @@ resource "libvirt_volume" "debian-bookworm-qcow2" {
   format = "qcow2"
   pool   = libvirt_pool.default.name # List storage pools using virsh pool-list
 }
-#
+
 resource "libvirt_volume" "debian-bookworm-qcow2-server" {
   name             = "debian-bookworm-server.qcow2"
-  base_volume_id   = libvirt_volume.debian-bookworm-qcow2.id
-  base_volume_pool = libvirt_volume.debian-bookworm-qcow2.pool
-}
-
-resource "libvirt_volume" "debian-bookworm-qcow2-agent0" {
-  name             = "debian-bookworm-agent0.qcow2"
   base_volume_id   = libvirt_volume.debian-bookworm-qcow2.id
   base_volume_pool = libvirt_volume.debian-bookworm-qcow2.pool
 }
@@ -50,27 +44,31 @@ resource "libvirt_cloudinit_disk" "commoninit" {
   user_data = data.template_file.user_data.rendered
 }
 
-resource "libvirt_domain" "k8s-server" {
-  name   = "k8s-server"
-  memory = "2048"
-  vcpu   = 2
-  network_interface {
-    network_name = libvirt_network.kube_network.name # List networks with virsh net-list
-  }
-  disk {
-    volume_id = libvirt_volume.debian-bookworm-qcow2-server.id
-  }
-  cloudinit = libvirt_cloudinit_disk.commoninit.id
-  console {
-    type        = "pty"
-    target_type = "serial"
-    target_port = "0"
-  }
-  graphics {
-    type        = "spice"
-    listen_type = "address"
-    autoport    = true
-  }
+module k8s-server {
+  source           = "../instance"
+  domain_name      = "debian-bookworm-server"
+  base_volume_id   = libvirt_volume.debian-bookworm-qcow2.id
+  base_volume_pool = libvirt_volume.debian-bookworm-qcow2.pool
+  network_name     = libvirt_network.kube_network.name
+  cloud_init_id    = libvirt_cloudinit_disk.commoninit.id
+}
+
+module k8s-agent-0 {
+  source           = "../instance"
+  domain_name      = "debian-bookworm-agent=0"
+  base_volume_id   = libvirt_volume.debian-bookworm-qcow2.id
+  base_volume_pool = libvirt_volume.debian-bookworm-qcow2.pool
+  network_name     = libvirt_network.kube_network.name
+  cloud_init_id    = libvirt_cloudinit_disk.commoninit.id
+}
+
+module k8s-agent-1 {
+  source           = "../instance"
+  domain_name      = "debian-bookworm-agent=1"
+  base_volume_id   = libvirt_volume.debian-bookworm-qcow2.id
+  base_volume_pool = libvirt_volume.debian-bookworm-qcow2.pool
+  network_name     = libvirt_network.kube_network.name
+  cloud_init_id    = libvirt_cloudinit_disk.commoninit.id
 }
 
 output "ips" {
