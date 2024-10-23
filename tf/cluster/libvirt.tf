3f05095b51223dcd299880b022ca6b633c846327
David Gallmeier <dg@xounges.net>
2024-10-23 17:37:36 -0400

    revert to default pool

--- tf/cluster/libvirt.tf
+++ tf/cluster/libvirt.tf
@@ -8,16 +8,16 @@ output homedir {
   value = null_resource.example
 }
 
-resource "libvirt_pool" "default" {
-  name = "k8s-pool"
-  type = "dir"
-
-  target {
-    path = "/home/davidg/k8s-pool"
-  }
-  # make sure ownership is set to libvirt-qemu:kvm
-  # permissions issues ref: https://github.com/dmacvicar/terraform-provider-libvirt/issues/658#issuecomment-569695035
-}
+# resource "libvirt_pool" "default" {
+#   name = "k8s-pool"
+#   type = "dir"
+#
+#   target {
+#     path = "/home/davidg/k8s-pool"
+#   }
+#   # make sure ownership is set to libvirt-qemu:kvm
+#   # permissions issues ref: https://github.com/dmacvicar/terraform-provider-libvirt/issues/658#issuecomment-569695035
+# }
 
 locals {
   k8s-nodes = {
@@ -45,14 +45,14 @@ locals {
   }
 }
 
-# module "k8s-nodes" {
-#   for_each         = local.k8s-nodes
-#   source           = "../instance"
-#   domain_name      = each.value.name
-#   hostname         = each.value.hostname
-#   fqdn             = each.value.fqdn
-#   ip-address       = each.value.ip
-#   base_volume_id   = libvirt_volume.debian-bookworm-qcow2.id
-#   base_volume_pool = libvirt_volume.debian-bookworm-qcow2.pool
-#   network_name     = libvirt_network.kube_network.name
-# }
+module "k8s-nodes" {
+  for_each         = local.k8s-nodes
+  source           = "../instance"
+  domain_name      = each.value.name
+  hostname         = each.value.hostname
+  fqdn             = each.value.fqdn
+  ip-address       = each.value.ip
+  base_volume_id   = libvirt_volume.debian-bookworm-qcow2.id
+  base_volume_pool = libvirt_volume.debian-bookworm-qcow2.pool
+  network_name     = libvirt_network.kube_network.name
+}
--- tf/cluster/volumes.tf
+++ tf/cluster/volumes.tf
@@ -3,5 +3,6 @@ resource "libvirt_volume" "debian-bookworm-qcow2" {
   #source = "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2"
   source = "file:///home/davidg/Downloads/debian-12-generic-amd64.qcow2"
   format = "qcow2"
-  pool   = libvirt_pool.default.name
+  #pool   = libvirt_pool.default.name
+  pool   = "default"
 }
